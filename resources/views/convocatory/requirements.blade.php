a97090bde171ec7e346d6a2c14c78a21f9486348
GustavoTch <GustavoTitoChoque1358923@gmail.com>
2020-05-17 08:34:24 -0400

    Implement UI "requisitos" version1

--- resources/views/convocatory/requirements.blade.php
+++ resources/views/convocatory/requirements.blade.php
@@ -5,50 +5,38 @@
     <!-- Contenido real de la página -->
     <div class="overflow-auto content">
         <h1>Requisitos</h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
+                
+<button type="button" class="btn btn-light add-item" data-toggle="modal" data-target="#exampleModalCenter">
+    <a class="add-item" data-toggle="modal" data-target="#exampleModalCenter">
+                    <img src="{{ asset('img/addBLUE.png')}}" width="35" height="35">
+                </a> Añadir requerimiento
+      </button>
+      
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">Añadir nuevo requisito</h5>
+              <button type="button" class="modal-icon" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Inciso A:</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Ingrese el requisito" rows="3"></textarea>
+                      <small id="emailHelp" class="form-text text-muted">Los requisitos son creados atravez de indices alfabeticos.</small>
+                    </div>
+                  </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-info">Guardar</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 @endsection
