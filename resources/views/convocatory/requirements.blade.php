71835646fa7c4b1bed259922033cd666d42ec3b8
GustavoTCh <42806689+GustavoTCh@users.noreply.github.com>
2020-05-17 10:32:37 -0400

    UI requeriments version 2

--- resources/views/convocatory/requirements.blade.php
+++ resources/views/convocatory/requirements.blade.php
@@ -4,16 +4,76 @@
     
     <!-- Contenido real de la página -->
     <div class="overflow-auto content">
-        <h1>Requisitos</h1>
-                
-<button type="button" class="btn btn-light add-item" data-toggle="modal" data-target="#exampleModalCenter">
-    <a class="add-item" data-toggle="modal" data-target="#exampleModalCenter">
+        <h5 style="margin: 20px" class="font-weight-bold">REQUISITOS</h5>
+        <br>
+        <div style="margin-left: 50px">
+            <button type="button" class="btn btn-light add-item" data-toggle="modal" data-target="#exampleModalCenter">
+                <a class="add-item" data-toggle="modal" data-target="#exampleModalCenter">
                     <img src="{{ asset('img/addBLUE.png')}}" width="35" height="35">
                 </a> Añadir requerimiento
-      </button>
-      
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            </button>
+            <div class="table-requests">
+                <table class="table">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Descripcion</th>
+                        <th scope="col">Opciones</th>
+                      </tr>
+                    </thead>
+                    <tbody style="background-color: white">
+                      <tr>
+                        <th scope="row">A</th>
+                        <td>Mark asdsad sadas sad asd sad  sad  asd  wq e wqe  wqe wqe wq e wq e  wq e 2 3 w q</td>
+                        <td style="text-align: center">
+                            <a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                                <img src="{{ asset('img/pen.png')}}" width="25" height="25">
+                            </a>
+                            <a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                                <img src="{{ asset('img/trash.png')}}" width="25" height="25">
+                            </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">B</th>
+                        <td>Mark</td>
+                        <td style="text-align: center">
+                            <a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                                <img src="{{ asset('img/pen.png')}}" width="25" height="25">
+                            </a>
+                            <a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                                <img src="{{ asset('img/trash.png')}}" width="25" height="25">
+                            </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">C</th>
+                        <td>Mark</td>
+                        <td style="text-align: center">
+                            <a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                                <img src="{{ asset('img/pen.png')}}" width="25" height="25">
+                            </a>
+                            <a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                                <img src="{{ asset('img/trash.png')}}" width="25" height="25">
+                            </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                </table>
+                <form>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Nota(*)</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Ingrese la nota respectiva" rows="3"></textarea>
+                    </div>
+                </form>
+                <div style="text-align: center">
+                    <button type="button" class="btn btn-info">Siguiente</button>
+                </div>
+            </div>
+        </div>
+        
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -29,7 +89,7 @@
                       <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Ingrese el requisito" rows="3"></textarea>
                       <small id="emailHelp" class="form-text text-muted">Los requisitos son creados atravez de indices alfabeticos.</small>
                     </div>
-                  </form>
+                </form>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
