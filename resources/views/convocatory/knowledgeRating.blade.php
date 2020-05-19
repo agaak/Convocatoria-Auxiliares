42b21b8e9d5c11085f3a5332cbaa99f96c41dcc9
AsokaZk <ashok_miau@hotmail.com>
2020-05-19 18:08:31 -0400

    vista-knowledgeRating

--- resources/views/convocatory/knowledgeRating.blade.php
+++ resources/views/convocatory/knowledgeRating.blade.php
@@ -3,51 +3,50 @@
 @section('content-convocatory')
     <!-- Contenido real de la página -->
     <div class="overflow-auto content">
-        <h1>Calificación de conocimientos</h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. In aperiam nihil
-            error culpa necessitatibus quam autem fuga, eligendi libero similique
-            magni, vel doloribus perspiciatis sequi fugit assumenda itaque magnam laborum?
-        </p>
+        <h5 style="margin: 25px" class="font-weight-bold">Calificacion de Conocimientos</h5>
+
+        <!-- Table -->
+    <div class="table-requests">
+        <table class="table table-bordered" style="text-align: center">
+          <thead class="thead-dark">
+            <tr>
+              <th style="vertical-align: middle; font-weight: normal;" scope="col" rowspan="2">#</th>
+              <th style="vertical-align: middle; font-weight: normal;" scope="col" rowspan="2">Tematica</th>
+              <th style="font-weight: normal" scope="col" colspan="6">Codigo de Auxiliatura</th>
+            </tr>
+            <tr>
+              <th style="font-weight: normal" scope="col">LCO-ADM</th>
+              <th style="font-weight: normal" scope="col">LDS-ADM</th>
+              <th style="font-weight: normal" scope="col">LDS-AUX</th>
+              <th style="font-weight: normal" scope="col">LM-AUX</th>
+              <th style="font-weight: normal" scope="col">LM-ADM</th>
+              <th style="font-weight: normal" scope="col">LM-AUX</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="table-light">1</td>
+              <td class="table-light">ADM LINUX</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td><a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                  <img src="{{ asset('img/pen.png')}}" width="30" height="30">
+                </a>
+                <a class="options">
+                  <img src="{{ asset('img/trash.png')}}" width="30" height="30">
+                </a></td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+
+    
+    <div class="my-5 py-5 text-center">
+        <a href="{{ route('knowledgeRating') }}" class="btn btn-info" tabindex="-1" role="button" aria-disabled="true">Finalizar</a>
+    </div>
     </div>
 @endsection
