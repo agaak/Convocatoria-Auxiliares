c6353d74abcd4f1cf86576fd0d019f515e3405c9
AsokaZk <ashok_miau@hotmail.com>
2020-05-21 13:23:15 -0400

    modal-edit-aux

--- resources/views/convocatory/knowledgeRating.blade.php
+++ resources/views/convocatory/knowledgeRating.blade.php
@@ -5,12 +5,18 @@
     <div class="overflow-auto content">
         <h5 style="margin: 25px" class="font-weight-bold">Calificacion de Conocimientos</h5>
       <!-- Button trigger modal -->
-      <button type="button" style="margin-left: 15px" class="btn add-item" data-toggle="modal" data-target="#exampleModalCenter">
+      <div class="row">
+      <button type="button" style="margin-left: 25px" class="btn add-item" data-toggle="modal" data-target="#exampleModalCenter">
         <a data-toggle="modal" data-target="#exampleModalCenter">
           <img src="{{ asset('img/addBLUE.png')}}" width="35" height="35">
         </a> Añadir Tematica
       </button>
-
+      <button type="button" style="margin-left: 15px" class="btn add-item" data-toggle="modal" data-target="#modalAuxiliatura">
+        <a data-toggle="modal" data-target="#modalAuxiliatura">
+          <img src="{{ asset('img/pen.png')}}" width="35" height="35">
+        </a> Editar Auxiliatura
+      </button>
+    </div>
         <!-- Table -->
     <div class="table-requests">
         <table class="table table-bordered" style="text-align: center">
@@ -33,7 +39,39 @@
           <tbody>
             <tr>
               <td class="table-light">1</td>
-              <td class="table-light">ADM LINUX</td>
+              <td class="table-light">Linux Avanzado</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light"><a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                  <img src="{{ asset('img/pen.png')}}" width="30" height="30">
+                </a>
+                <a class="options">
+                  <img src="{{ asset('img/trash.png')}}" width="30" height="30">
+                </a></td>
+            </tr>
+            <tr>
+              <td class="table-light">2</td>
+              <td class="table-light">Programacion web</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light">0</td>
+              <td class="table-light"><a class="options" data-toggle="modal" data-target="#exampleModalCenter">
+                  <img src="{{ asset('img/pen.png')}}" width="30" height="30">
+                </a>
+                <a class="options">
+                  <img src="{{ asset('img/trash.png')}}" width="30" height="30">
+                </a></td>
+            </tr>
+            <tr>
+              <td class="table-light">3</td>
+              <td class="table-light">Dase de datos</td>
               <td class="table-light">0</td>
               <td class="table-light">0</td>
               <td class="table-light">0</td>
@@ -85,6 +123,99 @@
           </form>
         </div>
       </div></div></div>
+         <!-- Modal Auxiliatura-->
+  <div class="modal fade" id="modalAuxiliatura" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Auxiliatura</h5>
+          <button type="button" class="modal-icon" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form method="POST" action="{{ route('request') }}">
+            {{ csrf_field() }}
+            <div class="form-group">
+              <div class="form-row">
+              <label class="col-auto col-form-label" for="department-conv">Auxiliatura</label>
+                <div class="col-xl">
+                    <select class="form-control" id="department-conv" name="departamento-ant">
+                        @php
+                            function valor($dato) {
+                                $direction = '';
+                                if ( old('departamento-ant') == $dato ) {
+                                    $direction = 'selected';
+                                }
+                            return $direction;
+                            }
+                        @endphp
+                        <option {{ valor('LCO-ADM') }}>LCO-ADM</option>
+                        <option {{ valor('LDS-ADM') }}>LDS-ADM</option>
+                        <option {{ valor('LDS-AUX') }}>LDS-AUX</option>
+                        <option {{ valor('LM-ADM') }}>LM-ADM</option>
+                        <option {{ valor('LM-AUX') }}>LM-AUX</option>
+                    </select>
+                </div>
+              </div>
+              <div class="form-row" style="margin-top: 20px">
+                  <div class="form-group col-7">
+                  <div class="row">
+                    <label for="marca colFormLabelSm" class="col-sm-5 col-form-label">Tematica 1:</label>
+                    <label for="marca colFormLabelSm" class="col-sm-7 col-form-label">Linux Avanzado</label>
+                  </div>
+                </div>
+                  <div class="form-group col-4">
+                  <div class="row">
+                    <label for="precio colFormLabelSm" class="col-sm-6 col-form-label">Porcentaje:</label>
+                    <div class="col-sm-6">
+                    <input name="precio" type="text" class="form-control form-control-sm" id="cod-aux" placeholder="30" required>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-7">
+                  <div class="row">
+                    <label for="marca colFormLabelSm" class="col-sm-5 col-form-label">Tematica 2:</label>
+                    <label for="marca colFormLabelSm" class="col-sm-7 col-form-label">Programacion Web</label>
+                  </div>
+                </div>
+                  <div class="form-group col-4">
+                  <div class="row">
+                    <label for="precio colFormLabelSm" class="col-sm-6 col-form-label">Porcentaje:</label>
+                    <div class="col-sm-6">
+                    <input name="precio" type="text" class="form-control form-control-sm" id="cod-aux" placeholder="30" required>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-7">
+                  <div class="row">
+                    <label for="marca colFormLabelSm" class="col-sm-5 col-form-label">Tematica 3:</label>
+                    <label for="marca colFormLabelSm" class="col-sm-7 col-form-label">Base de datos</label>
+                  </div>
+                </div>
+                  <div class="form-group col-4">
+                  <div class="row">
+                    <label for="precio colFormLabelSm" class="col-sm-6 col-form-label">Porcentaje:</label>
+                    <div class="col-sm-6">
+                    <input name="precio" type="text" class="form-control form-control-sm" id="cod-aux" placeholder="30" required>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+              </fieldset>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+              <input class="btn btn-info" type="submit" value="Guardar"></input>
+            </div>
+          </form>
+        </div>
+      </div></div>
     <div class="my-5 py-5 text-center">
         <a href="{{ route('knowledgeRating') }}" class="btn btn-info" tabindex="-1" role="button" aria-disabled="true">Finalizar</a>
     </div>
