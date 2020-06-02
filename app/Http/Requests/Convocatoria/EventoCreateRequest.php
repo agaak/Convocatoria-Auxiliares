a5a0a7999fc9c96e997b63912169e1b699f9ed1c
GustavoTCh <42806689+GustavoTCh@users.noreply.github.com>
2020-06-02 06:11:36 -0400

    Eventos error message

--- app/Http/Requests/Convocatoria/EventoCreateRequest.php
+++ app/Http/Requests/Convocatoria/EventoCreateRequest.php
@@ -31,7 +31,21 @@ class EventoCreateRequest extends FormRequest
             'titulo-evento' => 'required|unique:evento,titulo_evento',
             'lugar-evento' => 'required|unique:evento,lugar_evento',
             'fecha-ini-evento' => 'required|date|date_format:"Y-m-d\TH:i"|after_or_equal:'.$fechaIniConv,
-            'fecha-fin-evento' => 'required|date|date_format:"Y-m-d\TH:i"|after:fecha-ini-evento|before_or_equal:'.$fechaFinConv,
+            'fecha-fin-evento' => 'required|date|date_format:"Y-m-d\TH:i"|after_or_equal:fecha-ini-evento|before_or_equal:'.$fechaFinConv,
+        ];
+    }
+
+    public function messages()
+    {
+        $convActual = Convocatoria::where('id', request()->session()->get('convocatoria'));
+        $fechaIniConv = $convActual->value('fecha_inicio').' 00:00:00';
+        $fechaFinConv = $convActual->value('fecha_final').' 23:59:59';
+        return [
+            'fecha-ini-evento-evento.required' => 'El campo Fecha Inicio es obligatorio.',
+            'fecha-fin-evento-evento.required' => 'El campo Fecha Final es obligatorio.',
+            'fecha-ini-evento-evento.after_or_equal' => 'La Fecha Inicio debe ser una fecha posterior o igual a '.$fechaIniConv.'.',
+            'fecha-fin-evento-evento.before_or_equal' => 'La Fecha Final debe ser una fecha anterior o igual a '.$fechaFinConv.'.',
+            'fecha-fin-evento-evento.after_or_equal' => 'La Fecha Final debe ser una fecha posterior o igual a la fecha inicial.'
         ];
     }
 }
