f6ca2a331d04169ee3707b829aba4841ae634466
AdanFT <franco.start.moba@gmail.com>
2020-06-22 20:07:40 -0400

    agregando funcionalidades a la barra de navegacion para evaluador

--- resources/views/evaluador/evaluador.blade.php
+++ resources/views/evaluador/evaluador.blade.php
@@ -7,6 +7,9 @@
             <div class="card-personal">
                 <a class="card-personal-title" href="{{ route('helper.redirect', $conv->id) }}">{{ $conv->titulo }}</a>
                 <p class="card-personal-body">{{ $conv->descripcion_convocatoria }}</p>
+                <div class="text-center mb-1">
+                    <a href="{{ route('helper.redirect.ver', $conv->id) }}" class="btn btn-success btn-sm text-white">Ver</a>
+                </div>
             </div>
         @endforeach
     </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -96,6 +96,21 @@
                                     </li>
                                 </ul>
                             </div>
+                        @else 
+                            <a class="navbar-brand" href="#"></a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse navbar-collapse-color" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ activeMenu('convocatoria') }}" href="{{ route('convocatoria.index') }}">Convocatorias</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ activeMenu('evaluador*') }}" href="{{ route('evaluador.index') }}">Evaluar</a>
+                                    </li>
+                                </ul>
+                            </div>
                         @endif
                     @else
                         <a class="navbar-brand" href="#"></a>
