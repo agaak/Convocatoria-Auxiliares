5320511fd6402bea5affcb0650bc3ddc8299534d
AdanFT <franco.start.moba@gmail.com>
2020-06-22 20:46:47 -0400

    agregando nuevo nombre a las convocatorias del evaluador

--- app/Http/Controllers/Evaluador/EvaluadorController.php
+++ app/Http/Controllers/Evaluador/EvaluadorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Evaluador;
 
 use App\EvaluadorConocimientos;
 use App\Http\Controllers\Controller;
+use App\Tipo;
 
 class EvaluadorController extends Controller
 {
@@ -13,6 +14,7 @@ class EvaluadorController extends Controller
     }
     
     public function index() {
+
         session()->forget('convocatoria');
         $convs = EvaluadorConocimientos::where('correo', auth()->user()->email)->first()->convocatorias;
         session()->put('evaluador', $convs[0]['pivot']['id_evaluador']);
