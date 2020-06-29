745a5ac1c1c489b9110f7181836c7421a0da1072
AsokaZk <ashok_miau@hotmail.com>
2020-06-29 16:36:20 -0400

    vista-asignacionItems

--- app/Http/Controllers/AdmConvocatoria/AdmAsignacionController.php
+++ app/Http/Controllers/AdmConvocatoria/AdmAsignacionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\AdmConvocatoria;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Auxiliatura;
+use App\Models\Postulante;
 
 class AdmAsignacionController extends Controller
 {
@@ -14,8 +16,22 @@ class AdmAsignacionController extends Controller
     
     public function index()
     {   
+        $id_conv = session()->get('convocatoria');
         
-        return view('admConvocatoria.admAsignacionItems');
+        $listaAux = Auxiliatura::select('auxiliatura.nombre_aux','auxiliatura.id')
+        ->join('requerimiento','auxiliatura.id','=','requerimiento.id_auxiliatura')
+        ->where('id_convocatoria',$id_conv)
+        ->get();
+        $listaPost = Postulante::select('postulante.nombre','postulante.apellido','postulante.ci','postulante.id',
+        'postulante_auxiliatura.id_auxiliatura')
+        ->join('postulante_auxiliatura','postulante.id','=','postulante_auxiliatura.id_postulante')
+        ->join('postulante_conovocatoria','postulante.id','=','postulante_conovocatoria.id_postulante')
+        ->where('postulante_conovocatoria.id_convocatoria',$id_conv)
+        ->where('postulante_auxiliatura.habilitado', true)
+        ->groupby('postulante_auxiliatura.id_auxiliatura','postulante.id')
+        ->get();
+        $listaPost = collect($listaPost)->groupBy('id_auxiliatura');
+        return view('admConvocatoria.admAsignacionItems',compact('listaAux','listaPost'));
     }
 
 }
--- resources/views/admConvocatoria/admAsignacionItems.blade.php
+++ resources/views/admConvocatoria/admAsignacionItems.blade.php
@@ -2,7 +2,82 @@
 
 @section('content-adm-convocatoria')
 <div class="overflow-auto content">
-    <h1>Asignacion de items</h1>
+    <h3>Asignacion de items</h3>
+    <div class="container">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        @php $initTabs = true @endphp
+        @foreach ($listaAux as $auxiliatura)
+            <li class="nav-item">
+              <a class="nav-link{{ $initTabs ? " active" : '' }}" id={{ $auxiliatura->id }} data-toggle="tab" 
+                href="#{{ "body".$auxiliatura->id }}" role="tab" aria-controls="home" aria-selected={{ $initTabs }}>
+                {{ $auxiliatura->nombre_aux }}
+              </a>
+            </li>
+            {{ $initTabs = false  }}
+            @endforeach
+      </ul>
+      @php $initContent = true; @endphp
+      <div class="tab-content" id="myTabContent">
+        @foreach ($listaAux as $auxiliatura)
+          <div class="tab-pane fade{{ $initContent ? " show active" : '' }}" id={{ "body".$auxiliatura->id}} 
+            role="tabpanel" aria-labelledby={{ $auxiliatura->id}}>
+              <div class="table-requests1">
+                <table id= "notas{{ $auxiliatura->id}}" class="table table-bordered">
+                  <thead class="thead-dark text-left">
+                    <tr>
+                      <th class="font-weight-normal" scope="col">Item</th>
+                      <th class="font-weight-normal" scope="col">CI</th>
+                      <th class="font-weight-normal" scope="col">Apellidos</th>
+                      <th class="font-weight-normal" scope="col">Nombres</th>
+                      <th class="font-weight-normal" scope="col">Nota final</th>
+                      <th class="font-weight-normal" scope="col">Cantidad Items</th>
+                      <th class="font-weight-normal" scope="col">Asignar</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      @php $num=1; @endphp
+                    @if($listaPost->has($auxiliatura->id))
+                      @foreach ($listaPost[$auxiliatura->id] as $item)
+                        <tr>
+                          <td>{{ $num++ }}</td>
+                          <td>{{ $item->ci }}</td>
+                          <td>{{ $item->apellido }}</td>
+                          <td>{{ $item->nombre }}</td>
+                          <td>-</td>
+                          <td>1</td>
+                          <td>
+                            <form class="d-inline" action="#" method="POST">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-link">
+                                  <img src="{{ asset('img/add512.png') }}" width="30" height="32">
+                                </button>
+                              </form>
+                              <form class="d-inline" action="#" method="POST">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-link">
+                                  <img src="{{ asset('img/minus512.png') }}" width="30" height="32">
+                                </button>
+                              </form>
+                              <form class="d-inline" action="#" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button type="submit" class="btn btn-link">
+                                  <img src="{{ asset('img/trash.png') }}" width="25" height="30">
+                                </button>
+                              </form>
+                    </td>
+                          </tr>
+                      @endforeach
+                    @endif
+                  </tbody>   
+                </table>
+              </div>
+          </div>
+          {{ $initContent = false  }}
+          @endforeach
+      </div>
+  </div>
 </div>
     
 @endsection
