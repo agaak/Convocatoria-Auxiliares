d638f6eb10ebe37280ed099eed4bd1941da2b26f
he-ale <he.ale.cladera@gmail.com>
2020-06-29 19:54:54 -0400

    correcion1

--- app/Http/Controllers/VerConvocatoria/NotasMeritoController.php
+++ app/Http/Controllers/VerConvocatoria/NotasMeritoController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers\VerConvocatoria;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Postulante;
 
 class NotasMeritoController extends Controller
 {
     public function index() {
-        return view('verConvocatoria.notasMerito');
+        $postulantes= Postulante::select('postulante.nombre', 'postulante.apellido', 'postulante.ci', 'postulante.id', 'calf_final_postulante_merito.nota_final_merito as nota')
+        ->join('calf_final_postulante_merito', 'calf_final_postulante_merito.id_postulante', '=', 'postulante.id')
+        ->where('calf_final_postulante_merito.id_convocatoria', session()->get('convocatoria'))
+        ->join('postulante_auxiliatura','postulante.id','=','postulante_auxiliatura.id_postulante')
+        ->where('postulante_auxiliatura.habilitado',true)
+        ->orderBy('postulante.apellido','ASC')
+        ->get() ;
+        $postulantes = collect($postulantes)->unique('id');
+        return view('verConvocatoria.notasMerito', compact('postulantes'));
     }
 }
--- resources/views/verConvocatoria/notasMerito.blade.php
+++ resources/views/verConvocatoria/notasMerito.blade.php
@@ -3,5 +3,7 @@
 @section('content-resultados')
     <div class="overflow-auto content">
         <h1>NOTAS MERITO</h1>
+        @component('components.calificaciones.tablaPostulantesMeritos',['postulantes'=>$postulantes])
+        @endcomponent
     </div>
 @endsection
