dd725d83cc0a13218b100d0cf812f626921ca368
GustavoTCh <42806689+GustavoTCh@users.noreply.github.com>
2020-06-29 20:53:45 -0400

    fix multi datatable

--- resources/views/components/calificaciones/tablaNotasFinales.blade.php
+++ resources/views/components/calificaciones/tablaNotasFinales.blade.php
@@ -1,23 +1,3 @@
-
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }} ">
-    <link rel="stylesheet" href="{{ asset('css/styles.css')}}">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-
-    {{-- Datatables --}}
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.21/b-1.6.2/b-html5-1.6.2/rg-1.1.2/datatables.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/rowgroup/1.1.2/css/rowGroup.bootstrap4.min.css">
-    
-</head>
-<body>
   <div class="container">
     <!-- Nav tabs -->
     <ul class="nav nav-tabs" id="myTab" role="tablist">
@@ -77,14 +57,6 @@
       </div>
   </div>
     <script src="{{ asset('js/jquery-3.5.1.slim.min.js') }}"></script>
-    <script src="{{ asset('js/popper.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/es.js') }}"></script>
-    <script src="{{ asset('js/scripts.js') }}"></script>
-
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
     @foreach ($listaAux as $auxiliatura)
     <script>
       $(document).ready(function() {
@@ -109,7 +81,3 @@
       
   </script>
     @endforeach 
-
-    
-</body>
-</html>
