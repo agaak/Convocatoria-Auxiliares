5e1ad05017367a9b6c2348e04fd0fe687312f954
GustavoTCh <42806689+GustavoTCh@users.noreply.github.com>
2020-07-06 12:17:37 -0400

    implemeting datatable for notasByAuxiliatura

---
+++ resources/views/components/resultados/notasByAuxiliatura.blade.php
@@ -0,0 +1,72 @@
+<div class="container">
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+      @php $initTabs = true @endphp
+      @foreach ($listaAux as $auxiliatura)
+        <li class="nav-item">
+          <a class="nav-link{{ $initTabs ? " active" : '' }}" id={{ $auxiliatura->id }} data-toggle="tab" 
+              href="#{{ "body".$auxiliatura->id }}" role="tab" aria-controls="home" aria-selected={{ $initTabs }}>
+            {{ $auxiliatura->nombre_aux }}
+          </a>
+        </li>
+       {{ $initTabs = false  }}
+      @endforeach
+  </ul>
+  @php $initContent = true; @endphp
+  <div class="tab-content" id="myTabContent">
+      @foreach ($listaAux as $auxiliatura)
+        <div class="tab-pane fade{{ $initContent ? " show active" : '' }}" id={{ "body".$auxiliatura->id}} 
+          role="tabpanel" aria-labelledby={{ $auxiliatura->id}}>
+          <div class="table-requests1">
+              <table id= "notas{{ $auxiliatura->id}}" class="table table-striped table-bordered">
+                  <thead class="thead-dark text-left">
+                      <tr>
+                          <th class="font-weight-normal" scope="col">CI</th>
+                          <th class="font-weight-normal" scope="col">Nombre completo</th>
+                          @foreach ($auxiliatura->tematicas as $tematica)
+                          
+                          <th class="font-weight-normal" scope="col">{{ $tematica->nombre }}</th>
+                              
+                          @endforeach
+                        
+                          <th class="font-weight-normal" scope="col">Nota final</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                    @if($listaPost->has($auxiliatura->id))
+                      @foreach ($listaPost[$auxiliatura->id] as $item)
+                      <tr>
+                              <th style="font-weight: normal">{{ $item->ci }}</th>
+                              <th style="font-weight: normal">{{ $item->apellido }} {{ $item->nombre }}</th>
+                                @foreach ($item->notas_tems as $tems)
+                                <th class="font-weight-normal" scope="col">{{ $tems->calificacion??'-' }}</th>
+                                @endforeach
+                              <th style="font-weight: normal">{{ $item->nota_final_conoc??'-' }}</th>
+                              </tr>
+                      @endforeach
+                    @endif
+                  </tbody>
+              </table>
+          </div>
+        </div>
+        @php $initContent = false @endphp
+      @endforeach
+  </div>
+</div>
+
+<script src="{{ asset('js/jquery-3.5.1.slim.min.js') }}"></script>
+@foreach ($listaAux as $auxiliatura)
+  <script>
+    $(document).ready(function() {
+        $('#notas{{ $auxiliatura->id}}').DataTable({
+        
+          "pageLength":50,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+        },"bLengthChange": false,responsive: true,
+        order: [[2, 'asc']],"bPaginate": false
+        });
+      
+    });
+</script>
+@endforeach 
--- resources/views/verConvocatoria/notasConocimientoA.blade.php
+++ resources/views/verConvocatoria/notasConocimientoA.blade.php
@@ -3,64 +3,8 @@
 @section('content-resultados')
     <div class="overflow-auto content">
         <h1>NOTAS CONOCIMIENTO AUXILIATURA</h1>
-        
-    <div class="container">
-      <!-- Nav tabs -->
-      <ul class="nav nav-tabs" id="myTab" role="tablist">
-          @php $initTabs = true @endphp
-          @foreach ($listaAux as $auxiliatura)
-            <li class="nav-item">
-              <a class="nav-link{{ $initTabs ? " active" : '' }}" id={{ $auxiliatura->id }} data-toggle="tab" 
-                  href="#{{ "body".$auxiliatura->id }}" role="tab" aria-controls="home" aria-selected={{ $initTabs }}>
-                {{ $auxiliatura->nombre_aux }}
-              </a>
-            </li>
-           {{ $initTabs = false  }}
-          @endforeach
-      </ul>
-      @php $initContent = true; @endphp
-      <div class="tab-content" id="myTabContent">
-          @foreach ($listaAux as $auxiliatura)
-            <div class="tab-pane fade{{ $initContent ? " show active" : '' }}" id={{ "body".$auxiliatura->id}} 
-              role="tabpanel" aria-labelledby={{ $auxiliatura->id}}>
-              <div class="table-requests1">
-                  <table id= "table_id" class="table table-striped table-bordered">
-                      <thead class="thead-dark text-left">
-                          <tr>
-                              <th class="font-weight-normal" scope="col">CI</th>
-                              <th class="font-weight-normal" scope="col">Nombre completo</th>
-                              @foreach ($auxiliatura->tematicas as $tematica)
-                              
-                              <th class="font-weight-normal" scope="col">{{ $tematica->nombre }}</th>
-                                  
-                              @endforeach
-                            
-                              <th class="font-weight-normal" scope="col">Nota final</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                        @if($listaPost->has($auxiliatura->id))
-                          @foreach ($listaPost[$auxiliatura->id] as $item)
-                          <tr>
-                                  <th style="font-weight: normal">{{ $item->ci }}</th>
-                                  <th style="font-weight: normal">{{ $item->apellido }} {{ $item->nombre }}</th>
-                                    @foreach ($item->notas_tems as $tems)
-                                    <th class="font-weight-normal" scope="col">{{ $tems->calificacion??'-' }}</th>
-                                    @endforeach
-                                  <th style="font-weight: normal">{{ $item->nota_final_conoc??'-' }}</th>
-                                  </tr>
-                          @endforeach
-                        @endif
-                      </tbody>
-                      
-                  </table>
-              </div>
-            </div>
-            @php $initContent = false @endphp
-          @endforeach
-          
-      </div>
-</div>
-</div>
+        @component('components.resultados.notasByAuxiliatura', 
+          ['listaAux' => $listaAux,'listaPost'=>$listaPost])
+        @endcomponent
     </div>
 @endsection
