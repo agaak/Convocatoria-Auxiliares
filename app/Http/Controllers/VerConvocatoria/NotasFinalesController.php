979aa43d4112b06da6c59225b7a53ef99c106c0e
AsokaZk <ashok_miau@hotmail.com>
2020-07-06 22:04:08 -0400

    notas-finales-publico

--- app/Http/Controllers/VerConvocatoria/NotasFinalesController.php
+++ app/Http/Controllers/VerConvocatoria/NotasFinalesController.php
@@ -11,6 +11,7 @@ use App\Models\Convocatoria;
 use App\Models\Calificacion_final;
 use App\Models\Postulante_auxiliatura;
 use App\Models\Postulante_conovocatoria;
+use App\Models\PostuCalifConocFinal;
 
 class NotasFinalesController extends Controller
 {
@@ -28,22 +29,22 @@ class NotasFinalesController extends Controller
         ->where('id_convocatoria',$id_conv)
         ->get();
         $listaPost = Postulante::select('postulante.nombre','postulante.apellido','postulante.ci','postulante.id',
-        'calf_fin_postulante_conoc.nota_final_conoc','calf_final_postulante_merito.nota_final_merito','postulante_auxiliatura.id_auxiliatura','postulante_auxiliatura.calificacion as not_fin')
-        ->join('postulante_auxiliatura','postulante.id','=','postulante_auxiliatura.id_postulante')
-        ->join('postulante_conovocatoria','postulante.id','=','postulante_conovocatoria.id_postulante')
-        ->where('postulante_conovocatoria.id_convocatoria',$id_conv)
-        ->join('calf_fin_postulante_conoc','postulante.id','=','calf_fin_postulante_conoc.id_postulante')
+                'calf_final_postulante_merito.nota_final_merito as nota_fin_merit')
         ->join('calf_final_postulante_merito','postulante.id','=','calf_final_postulante_merito.id_postulante')
-        ->where('postulante_auxiliatura.habilitado', true)
-        ->groupby('postulante_auxiliatura.id_auxiliatura','postulante.id','calf_fin_postulante_conoc.nota_final_conoc','calf_final_postulante_merito.nota_final_merito','not_fin')
+        ->where('calf_final_postulante_merito.id_convocatoria',$id_conv)
         ->get();
-
         foreach($listaPost as $post){
-            $post->nota_final_conoc = number_format($post->nota_final_conoc*$porcentaje_conoc/100 ,2);
-            $post->nota_final_merito = number_format($post->nota_final_merito*$porcentaje_merit/100 ,2);
+            $post->nota_fin_merit = number_format($post->nota_fin_merit*$porcentaje_merit/100 ,2);
+            $notas = Postulante_auxiliatura::select('calificacion as nota_fin','id_auxiliatura')
+                ->where('id_postulante',$post->id)->get();
+            foreach($notas as $aux){
+                $aux->nota_fin_conoc = PostuCalifConocFinal::where('id_postulante',$post->id)
+                    ->where('id_auxiliatura',$aux->id_auxiliatura)->value('nota_final_conoc');
+                $aux->nota_fin_conoc = number_format($aux->nota_fin_conoc*$porcentaje_conoc/100 ,2); 
+            }
+            $notas = collect($notas)->groupBy('id_auxiliatura');
+            $post->aux_conoc = $notas;
         }
-        $listaPost = collect($listaPost)->groupBy('id_auxiliatura');
-        
         $conv = Convocatoria::find(session()->get('convocatoria'));
 
         return view('verConvocatoria.notasFinales',compact('listaAux','listaPost','titulo_conv',
